ec0c3b649501190f2e8dab8bf123aaaf15a602ad
A6ish3k <abhishek199507028@gmail.com>
2022-12-07 00:52:08 +0000

    Created login and sigh up and connected to firebase.

---
+++ app/src/main/java/com/example/signupapp/SplashScreen.java
@@ -0,0 +1,47 @@
+package com.example.signupapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class SplashScreen extends AppCompatActivity {
+
+    Animation topAnim, bottomAnim;
+    ImageView image;
+    TextView slogan;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash_screen);
+
+        //Animations
+        topAnim = AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        bottomAnim = AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
+
+        //Hooks
+        image = findViewById(R.id.imageView3);
+        slogan = findViewById(R.id.textview);
+
+        image.setAnimation(topAnim);
+        slogan.setAnimation(bottomAnim);
+
+        int SPLASH_SCREEN = 2000;
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(SplashScreen.this,SignupActivity.class);
+            startActivity(intent);
+            finish();
+        }, SPLASH_SCREEN);
+
+    }
+}
